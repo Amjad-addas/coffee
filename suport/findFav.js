fa761a534fb992d8dc11e1935b4ad6835d275f32
unknown <mo.itmo25@gmail.com>
2025-09-29 15:29:23 +0300

    first commit

---
+++ suport/findFav.js
@@ -0,0 +1,19 @@
+const Favourit = require("../modle/favourit")
+const decode = require("./decodJWT")
+
+const isFav=async(req)=>{
+    let isfav=0
+
+    const userId=decode(req.cookies.id)
+    const productId=req.params.id
+        const fav = await Favourit.find({
+        userId:userId,
+        favouritId:productId
+    })
+    if(fav[0]){
+        isfav=1
+    }
+    return isfav
+}
+
+module.exports=isFav
